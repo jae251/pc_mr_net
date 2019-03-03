93cc77d0e1d7b691b31fb469054b22d07225f42c
jae251 <jae251@gmx.de>
2019-03-03 17:51:44 +0100

    add function to compute n nearest neighbours in point cloud

--- utilities/pcl_utils.py
+++ utilities/pcl_utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.spatial import distance
 
 
 def rotate_pcl(pcl, angle):
@@ -9,3 +10,23 @@ def rotate_pcl(pcl, angle):
                                     (0, 0, 1)))
     rotated_pcl = np.matmul(pcl, rotation_matrix_trp)
     return rotated_pcl
+
+
+def find_nearest_points(pcl, n):
+    '''
+    find indices and distances of n nearest points in point cloud
+    :param pcl: point cloud array
+    :param n: number of nearest neighbours to find
+    :return: array of nearest points, shape=(len(pcl),n,3),
+             array of distances, shape=(len(pcl),n)
+    '''
+    distance_matrix = distance.squareform(distance.pdist(pcl))
+    sorted_indices_by_distance = np.argsort(distance_matrix, axis=1)
+    nearest_neighbour_indices = sorted_indices_by_distance[:, 1:n + 1]  # first entry is self distance
+    row_indices = np.repeat(sorted_indices_by_distance[:, 0][:, np.newaxis], n, axis=1)
+
+    nearest_points = pcl[nearest_neighbour_indices]
+    # print(nearest_points.shape)
+    distances = distance_matrix[row_indices, nearest_neighbour_indices]
+    # print(distances.shape)
+    return nearest_points, distances

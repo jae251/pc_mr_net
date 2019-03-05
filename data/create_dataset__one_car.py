ae87fa1f2811dcf18e51dbdb613e34fe748917e3
jae251 <jae251@gmx.de>
2019-03-05 19:58:44 +0100

    adapting to linux env

--- data/create_dataset__one_car.py
+++ data/create_dataset__one_car.py
@@ -6,19 +6,21 @@ from lidar_simulation.lidar import Lidar
 from lidar_simulation.data_loaders.load_3d_models import load_obj_file
 from lidar_simulation.scene_builder import Scene
 
-from untitled1.utilities.utils import adapt_z_position
+from utilities.utils import adapt_z_position
 
 if not os.path.isdir("./dataset_one_car"):
     os.mkdir("dataset_one_car")
 
 scene = Scene(spawn_area=((-20, -5), (20, -5), (20, 20), (-20, 20)))
-obj_file = os.path.expanduser("~/Downloads/3d models/Porsche_911_GT2.obj")
+# obj_file = os.path.expanduser("~/Downloads/3d models/Porsche_911_GT2.obj")
+obj_file = "/mnt/sda2/Users/usr/Downloads/3d models/Porsche_911_GT2.obj"
 scene.add_model_to_shelf(*load_obj_file(obj_file), "Car")
 
-nr_of_scenes = 10000
+nr_of_scenes = 2000
 for scene_nr in range(nr_of_scenes):
     print(scene_nr)
-    filename = "./dataset_one_car/{:>05}.hdf5".format(scene_nr)
+    # filename = "./dataset_one_car/{:>05}.hdf5".format(scene_nr)
+    filename = "./dataset_one_car/eval/{:>05}.hdf5".format(scene_nr)
     if os.path.isfile(filename):
         continue
 

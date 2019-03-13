05f59b88095fc6a6a6350c62351667d376181c19
jae251 <jae251@gmx.de>
2019-03-13 22:16:24 +0100

    fix super call and concat dimension

--- layers/convolution_module.py
+++ layers/convolution_module.py
@@ -6,25 +6,25 @@ from layers.point_convolution import PointConvolution
 
 class ConvolutionModule(nn.Module):
     def __init__(self, input_channels, output_channels):
-        super.__init__()
-        self.conv3x3 = nn.Conv2d(input_channels, output_channels, 3)
-        self.conv5x5 = nn.Conv2d(input_channels, output_channels, 5)
+        super().__init__()
+        self.conv3x3 = nn.Conv2d(input_channels, output_channels, 3, padding=1)
+        self.conv5x5 = nn.Conv2d(input_channels, output_channels, 5, padding=2)
 
     def forward(self, x):
         x0 = nn.functional.relu(self.conv3x3(x))
         x1 = nn.functional.relu(self.conv5x5(x))
-        x = torch.cat([x0, x1])
+        x = torch.cat([x0, x1], 1)
         return x
 
 
 class FirstLayer(nn.Module):
     def __init__(self):
-        super.__init__()
+        super().__init__()
         self.conv3x3 = PointConvolution(3, 20)
         self.conv5x5 = PointConvolution(5, 20)
 
     def forward(self, x):
         x0 = nn.functional.relu(self.conv3x3(x))
         x1 = nn.functional.relu(self.conv5x5(x))
-        x = torch.cat([x0, x1])
+        x = torch.cat([x0, x1], 1)
         return x

ad96200d65628bf44d357941d4a8788008400a7a
jae251 <jae251@gmx.de>
2019-03-13 22:19:04 +0100

    change to MSEloss, adapt eval output to regression

--- layers/pc_mr_net.py
+++ layers/pc_mr_net.py
@@ -26,30 +26,31 @@ class PointCloudMapRegressionNet(nn.Module):
         return x
 
 
-EPOCHS = 10
-BATCHSIZE = 10
+EPOCHS = 20
 if __name__ == '__main__':
     t1 = time()
     net = PointCloudMapRegressionNet()
     net = net.cuda()
     optimizer = Adam(net.parameters())
-    loss_function = nn.CrossEntropyLoss()
+    loss_function = nn.MSELoss()
 
-    train_loader = HdfLoader("../data/dataset_one_car/train", shuffle=True, feature_distances=(.2, .4, .8))
-    eval_loader = HdfLoader("../data/dataset_one_car/eval", shuffle=False, feature_distances=(.2, .4, .8))
+    train_loader = HdfLoader("../data/dataset_one_car/train", shuffle=True)
+    eval_loader = HdfLoader("../data/dataset_one_car/eval", shuffle=False)
 
     print('==>>> total training batch number: {}'.format(len(train_loader)))
     print('==>>> total testing batch number: {}'.format(len(eval_loader)))
 
     # training loop
     for epoch in range(EPOCHS):
+        print("EPOCH ", epoch)
         for i, (input, labels) in enumerate(train_loader):
+            if i % 10 == 0:
+                print(i)
             optimizer.zero_grad()
             input = input.cuda()
             labels = labels.cuda()
 
             output = net(input)
-
             loss = loss_function(output, labels)
             loss.backward()
             optimizer.step()
@@ -57,19 +58,19 @@ if __name__ == '__main__':
     print("Elapsed training time: {}".format(t2 - t1))
 
     # evaluation loop
-    total_inputs = 0
-    correct_labels = 0
+    loss = 0
     for i, (input, labels) in enumerate(eval_loader):
+        if i % 10 == 0:
+            print(i)
         input = input.cuda()
         labels = labels.cuda()
 
         output = net(input)
-
-        _, predictions = torch.max(output.data, 1)
-        total_inputs += input.data.size()[0]
-        correct_labels += (predictions == labels.data).sum()
+        # mask = labels != 0
+        # mask = mask.any(1)
+        loss += float(loss_function(output, labels))
     t3 = time()
-    print("Elapsed evalutaion time: {}".format(t3 - t2))
-    print("Accuracy is {:.2f} %".format(correct_labels * 100.0 / total_inputs))
+    print("Elapsed evalutation time: {}".format(t3 - t2))
+    print("Average loss is: ", loss / i)
 
 # torch.save(model.state_dict(), model.name())

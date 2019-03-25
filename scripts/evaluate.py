81e5ba181a039db867a578ed59f6521194fe6b47
jae251 <jae251@gmx.de>
2019-03-25 18:53:49 +0100

    evaluate all models in net_weights folder and write metric summaries

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -3,8 +3,10 @@ import torch.nn as nn
 import os
 import numpy as np
 from time import time
+
 from scripts.train import PointCloudMapRegressionNet
 from data.hdf_dataset_loader import HdfDataset
+from utilities.metrics import EvaluationMetrics
 
 
 def custom_collate_fn(sample):
@@ -31,32 +33,40 @@ BATCHSIZE = 10
 NUM_WORKERS = 3
 
 if __name__ == '__main__':
-    net = PointCloudMapRegressionNet()
-    net.load_state_dict(torch.load("../net_weights/0109.pt"))
-    net = net.cuda()
-
-    eval_dataset = HdfDataset("../data/dataset_one_car/eval")
-    eval_loader = torch.utils.data.DataLoader(eval_dataset, batch_size=BATCHSIZE, shuffle=False,
-                                              collate_fn=custom_collate_fn, num_workers=NUM_WORKERS)
-
-    # evaluation loop
-    loss = 0
-    samples = 0
     t1 = time()
-    for i, (input, labels) in enumerate(eval_loader):
-        if i % 10 == 0:
-            print("\r{}".format(i), end="")
-        input = input.cuda()
-        labels = labels.cuda()
-
-        output = net(input)
-        mask = labels != 0
-        mask = mask.any(1)
-        average_error_distance = ((output - labels) ** 2).sum(1).sqrt()
-        average_error_distance = average_error_distance[mask]
-        samples += len(average_error_distance)
-        loss += float(average_error_distance.sum())
-    t2 = time()
+    tensorboard_metrics = EvaluationMetrics(log_dir="../eval_summaries")
+    loss_function = nn.MSELoss()
     print("")
+    for i, weight_file in enumerate(sorted(os.listdir("../net_weights"))):
+        print("\rEvaluating ", weight_file)
+        net = PointCloudMapRegressionNet()
+        net.load_state_dict(torch.load("../net_weights/" + weight_file))
+        net = net.cuda()
+
+        eval_dataset = HdfDataset("../data/dataset_one_car/eval")
+        eval_loader = torch.utils.data.DataLoader(eval_dataset, batch_size=BATCHSIZE, shuffle=False,
+                                                  collate_fn=custom_collate_fn, num_workers=NUM_WORKERS)
+
+        # evaluation loop
+        # loss = 0
+        # samples = 0
+        for i, (input, labels) in enumerate(eval_loader):
+            if i % 10 == 0:
+                print("\r{}".format(i), end="")
+            input = input.cuda()
+            labels = labels.cuda()
+
+            output = net(input).data
+            mask = labels != 0
+            mask = mask.any(1)
+            average_error_distance = ((output - labels) ** 2).sum(1).sqrt()
+            average_error_distance = average_error_distance[mask]
+            loss = loss_function(output, labels).item()
+            # samples += len(average_error_distance)
+            # loss += float(average_error_distance.sum())
+            tensorboard_metrics.add_batch(loss, output, labels)
+        tensorboard_metrics.create_summary(i)
+        # print("Average loss is: ", loss / samples)
+    t2 = time()
     print("Elapsed evalutation time: {}".format(t2 - t1))
-    print("Average loss is: ", loss / samples)
+    tensorboard_metrics.close()

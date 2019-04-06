aa8538d2ef841ece1cdc6c3fc57e5f8d850ca8d7
jae251 <jae251@gmx.de>
2019-04-06 18:01:04 +0200

    add 2d box/pcl visualization with plt

--- data/visualize.py
+++ data/visualize.py
@@ -1,12 +1,15 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from PIL import Image
 from h5py import File
 from pptk import viewer
+from utilities.bounding_boxes import BoundingBox
 
 
 def visualize(filepath):
     with File(filepath, "r") as f:
         pcl = np.array(f["point_cloud"])
+        bb = BoundingBox.from_numpy(f["bounding_boxes"][0])
     print(pcl.shape)
     viewer(pcl)
     distance_picture = np.linalg.norm(pcl, axis=2)
@@ -18,6 +21,11 @@ def visualize(filepath):
     distance_picture *= normalize_coeff
     im = Image.fromarray(distance_picture)
     im.show()
+    bb.draw_to_plot_2d(plt)
+    pcl = pcl.reshape(-1, 3)
+    plt.scatter(pcl[:, 0], pcl[:, 1], c="g", s=.1)
+    plt.gca().set_aspect(aspect=1)
+    plt.show()
 
 
 if __name__ == '__main__':
